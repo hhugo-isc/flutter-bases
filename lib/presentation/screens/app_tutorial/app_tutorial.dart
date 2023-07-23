2c686872804375c6e18923e4dce90177a3787c28
lhhernandez <hhernandez.isc@gmail.com>
2023-07-22 22:34:32 -0600

    Screen creadas y barril actualizado

---
+++ lib/presentation/screens/app_tutorial/app_tutorial.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String name = "Tutorial Screen";
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  static const String name = "UiControlsScreen";
+  const UiControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
